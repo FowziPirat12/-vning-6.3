6b39b391eeb440d51f2f3a04803dd817a0ef96f1
melvinsjostrand <111962656+Dungeater1337@users.noreply.github.com>
2023-02-03 10:52:46 +0100

    Add project files.

---
+++ Övning 6.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Övning_6._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            SkrivTalKvadratMelllan(1, 10);
+        }
+        static void SkrivTalKvadratMelllan(int NedreGräns, int ÖvreGräns)
+        {
+            for(int i= NedreGräns; i <= ÖvreGräns; i++)
+            {
+                SkrivTalKvadrat(i);
+            }
+        }
+        static void SkrivTalKvadrat(int tal)
+        {
+            Console.WriteLine($"talet {tal} i kvadrat är: {tal * tal}");
+        }
+    }
+}
